b76b95d1a0f931e7b8f9d6f14945754adbe5f524
ciricbogdan <ciric.bogdan314@gmail.com>
2019-04-13 01:58:15 +0200

    Added file manager module along with rename and new folder dialogs. No services provided.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  currentPath = '/'
+  
   title = 'exams-cloud-front';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FileExplorerModule } from './file-explorer/file-explorer.module';
+import { MatCardModule } from '@angular/material/card'
+import { NewFolderDialogComponent } from './file-explorer/modals/new-folder-dialog/new-folder-dialog.component';
+import { RenameDialogComponent } from './file-explorer/modals/rename-dialog/rename-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FileExplorerModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
